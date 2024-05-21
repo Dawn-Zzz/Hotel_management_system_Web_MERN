7a7efb44667d8ef78bbe6a0ea7b16e518c9f08b8
binh minh <ng.binhminh03@gmail.com>
2024-05-21 23:04:50 +0700

    Feat: View list room

--- frontend/src/admin/pages/room/rooms/Rooms.jsx
+++ frontend/src/admin/pages/room/rooms/Rooms.jsx
@@ -2,13 +2,16 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { viewListRoom } from "../../../../service/roomService";
 import Loading from "../../../../components/loading/Loading";
+import { viewListRoomType } from "../../../../service/roomTypeService";
 
 const Rooms = () => {
   const [data, setData] = useState([]);
+  const [roomTypes, setRoomTypes] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     getData();
+    getRoomTypes();
   }, []);
 
   const getData = async () => {
@@ -25,6 +28,25 @@ const Rooms = () => {
     } catch (error) {
       console.error(error);
     }
+  };
+
+  const getRoomTypes = async () => {
+    try {
+      const data = await viewListRoomType(1);
+      console.log(data);
+      if (data?.code === 0) {
+        setRoomTypes(data?.data);
+      } else {
+        setRoomTypes([]);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const getRoomTypeName = (roomTypeId) => {
+    const roomType = roomTypes.find((rt) => rt._id === roomTypeId);
+    return roomType ? roomType.name : "Unknown";
   };
 
   return isLoading ? (
@@ -44,18 +66,29 @@ const Rooms = () => {
       </div>
       <div className="mt-4 flex flex-wrap">
         {data.map((room) => (
-          <div className=" w-[24%] mt-4 bg-white rounded-lg border-l-4 border-[#1cc88a] mr-2">
+          <div
+            key={room._id}
+            className={`w-[24%] mt-4 bg-white rounded-lg border-l-4 mr-2 ${
+              room.isFree ? "border-[#1cc88a]" : "border-red-600"
+            }`}
+          >
             <div className="flex flex-col p-6">
               <div>
-                <p className="uppercase font-semibold text-lg text-[#1cc88a]">
-                  Family
+                <p
+                  className={`uppercase font-semibold text-lg ${
+                    room.isFree ? "text-[#1cc88a]" : "text-red-600"
+                  }`}
+                >
+                  {getRoomTypeName(room.roomType)}
                 </p>
                 <p className="font-semibold text-lg mt-4">{room.roomNumber}</p>
               </div>
               <div className="flex justify-end">
                 <Link
                   to="/admin/rooms/detail"
-                  className="text-white bg-[#1cc88a] py-2 px-6 rounded-full"
+                  className={`text-white py-2 px-6 rounded-full ${
+                    room.isFree ? "bg-[#1cc88a]" : "bg-red-600"
+                  }`}
                 >
                   Details
                 </Link>
@@ -63,61 +96,6 @@ const Rooms = () => {
             </div>
           </div>
         ))}
-
-        {/* <div className=" w-[24%] mt-4 bg-white rounded-lg border-l-4 border-[#1cc88a] mx-2">
-          <div className="flex flex-col p-6">
-            <div>
-              <p className="uppercase font-semibold text-lg text-[#1cc88a]">
-                Family
-              </p>
-              <p className="font-semibold text-lg mt-4">101</p>
-            </div>
-            <div className="flex justify-end">
-              <Link
-                to="/admin/rooms/detail"
-                className="text-white bg-[#1cc88a] py-2 px-6 rounded-full"
-              >
-                Details
-              </Link>
-            </div>
-          </div>
-        </div>
-        <div className=" w-[24%] mt-4 bg-white rounded-lg border-l-4 border-[#1cc88a] mx-2">
-          <div className="flex flex-col p-6">
-            <div>
-              <p className="uppercase font-semibold text-lg text-[#1cc88a]">
-                Family
-              </p>
-              <p className="font-semibold text-lg mt-4">101</p>
-            </div>
-            <div className="flex justify-end">
-              <Link
-                to="/admin/rooms/detail"
-                className="text-white bg-[#1cc88a] py-2 px-6 rounded-full"
-              >
-                Details
-              </Link>
-            </div>
-          </div>
-        </div>
-        <div className=" w-[24%] mt-4 bg-white rounded-lg border-l-4 border-red-600 ml-2">
-          <div className="flex flex-col p-6">
-            <div>
-              <p className="uppercase font-semibold text-lg text-red-600">
-                Family
-              </p>
-              <p className="font-semibold text-lg mt-4">101</p>
-            </div>
-            <div className="flex justify-end">
-              <Link
-                to="/admin/rooms/detail"
-                className="text-white bg-red-600 py-2 px-6 rounded-full"
-              >
-                Details
-              </Link>
-            </div>
-          </div>
-        </div> */}
       </div>
     </div>
   );
