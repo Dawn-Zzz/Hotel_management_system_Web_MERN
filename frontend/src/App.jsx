2d7998ee947c928de086e131eacfa37f1d377d40
PhucLee14 <lhphucth14@gmail.com>
2024-06-10 20:30:00 +0700

    add dotenv

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
 import {
-  BrowserRouter,
-  Routes,
-  Route,
-  useNavigate,
-  useLocation,
+    BrowserRouter,
+    Routes,
+    Route,
+    useNavigate,
+    useLocation,
 } from "react-router-dom";
 import { publicRoutes } from "./routes/Routes";
 import DefaultLayout from "./components/layouts/DefaultLayout";
@@ -16,76 +16,76 @@ import { useDispatch, useSelector } from "react-redux";
 import { handleRefresh } from "./redux/auth/authAction";
 
 function App() {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const location = useLocation();
-  const auth = useSelector((state) => state.auth);
-  // const authAdmin = useSelector((state) => state.authAdmin);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const location = useLocation();
+    const auth = useSelector((state) => state.auth);
+    // const authAdmin = useSelector((state) => state.authAdmin);
 
-  useEffect(() => {
-    if (window.location.pathname.startsWith("/admin")) {
-      dispatch(handleRefresh());
-    }
-  }, []);
+    useEffect(() => {
+        if (window.location.pathname.startsWith("/admin")) {
+            dispatch(handleRefresh());
+        }
+    }, []);
 
-  useEffect(() => {
-    if (auth.auth && location.pathname === "/admin/login") {
-      navigate("/admin");
-    }
+    useEffect(() => {
+        if (auth.auth && location.pathname === "/admin/login") {
+            navigate("/admin");
+        }
 
-    if (!auth.auth && window.location.pathname.startsWith("/admin")) {
-      navigate("/admin/login");
-    }
-  }, [auth.auth, location.pathname]);
+        if (!auth.auth && window.location.pathname.startsWith("/admin")) {
+            navigate("/admin/login");
+        }
+    }, [auth.auth, location.pathname]);
 
-  return (
-    <>
-      <Routes>
-        {publicRoutes.map((route, index) => {
-          const Page = route.component;
-          let Layout = DefaultLayout;
-          if (route.layout) {
-            Layout = route.Layout;
-          } else if (route.layout === null) {
-            Layout = Fragment;
-          }
-          return (
-            <Route
-              key={index}
-              path={route.path}
-              element={
-                <Layout>
-                  <Page />
-                </Layout>
-              }
-            />
-          );
-        })}
-        {publicAdminRoutes.map((route, index) => {
-          const Page = route.component;
-          let Layout = AdminDefaultLayout;
+    return (
+        <>
+            <Routes>
+                {publicRoutes.map((route, index) => {
+                    const Page = route.component;
+                    let Layout = DefaultLayout;
+                    if (route.layout) {
+                        Layout = route.Layout;
+                    } else if (route.layout === null) {
+                        Layout = Fragment;
+                    }
+                    return (
+                        <Route
+                            key={index}
+                            path={route.path}
+                            element={
+                                <Layout>
+                                    <Page />
+                                </Layout>
+                            }
+                        />
+                    );
+                })}
+                {publicAdminRoutes.map((route, index) => {
+                    const Page = route.component;
+                    let Layout = AdminDefaultLayout;
 
-          if (route.layout) {
-            Layout = route.layout;
-          } else if (route.layout === null) {
-            Layout = Fragment;
-          }
-          return (
-            <Route
-              key={index}
-              path={route.path}
-              element={
-                <Layout>
-                  <Page />
-                </Layout>
-              }
-            />
-          );
-        })}
-      </Routes>
-      <Toaster position="bottom-right" reverseOrder={false} />
-    </>
-  );
+                    if (route.layout) {
+                        Layout = route.layout;
+                    } else if (route.layout === null) {
+                        Layout = Fragment;
+                    }
+                    return (
+                        <Route
+                            key={index}
+                            path={route.path}
+                            element={
+                                <Layout>
+                                    <Page />
+                                </Layout>
+                            }
+                        />
+                    );
+                })}
+            </Routes>
+            <Toaster position="bottom-right" reverseOrder={false} />
+        </>
+    );
 }
 
 export default App;
--- frontend/src/utils/apiBackend.js
+++ frontend/src/utils/apiBackend.js
@@ -1,45 +1,35 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3001/",
-  withCredentials: true,
+    baseURL: import.meta.env.VITE_BASEURL || "http://localhost:3001/",
+    withCredentials: true,
 });
 
 instance.interceptors.response.use(
-  function (response) {
-    return response.data ? response.data : response;
-  },
-  function (error) {
-    if (error.response) {
-      // Xử lý lỗi từ response HTTP
-      const { status, data } = error.response;
+    function (response) {
+        return response.data ? response.data : response;
+    },
+    function (error) {
+        if (error.response) {
+            const { status, data } = error.response;
 
-      // Thông báo lỗi cho người dùng nếu cần thiết
-      if (status === 401) {
-        // Xử lý lỗi xác thực, chẳng hạn chuyển hướng đến trang đăng nhập
-        console.log("Unauthorized error:", data);
-      } else {
-        // Hiển thị thông báo lỗi cho người dùng
-        console.log("An error occurred:", data);
-      }
-    } else if (error.request) {
-      // Xử lý lỗi không có response từ server
-      console.log("No response received from server:", error.request);
-    } else {
-      // Xử lý lỗi khác (lỗi trong quá trình gửi request)
-      console.log("Error in request:", error.message);
+            if (status === 401) {
+                console.log("Unauthorized error:", data);
+            } else {
+                console.log("An error occurred:", data);
+            }
+        } else if (error.request) {
+            console.log("No response received from server:", error.request);
+        } else {
+            console.log("Error in request:", error.message);
+        }
+
+        return {
+            data: error.response ? error.response.data : null,
+            status: error.response ? error.response.status : null,
+            headers: error.response ? error.response.headers : null,
+        };
     }
-
-    // Ghi log hệ thống
-    // logErrorToServer(error);
-
-    // Trả về một object chứa thông tin lỗi
-    return {
-      data: error.response ? error.response.data : null,
-      status: error.response ? error.response.status : null,
-      headers: error.response ? error.response.headers : null,
-    };
-  }
 );
 
 export default instance;
